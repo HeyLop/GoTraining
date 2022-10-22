f3bcea19eda52dbccd43ec6cc06b4534c3991f38
haoyunpeng <haoyunpeng@hotmail.com>
2022-10-22 15:30:43 +0800

    2022-10-22

--- go_method/point_test.go
+++ go_method/point_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -17,6 +18,22 @@ func (ty *Tyt) M2() {
 	ty.i = 11
 }
 
+func dumpMethodSet(i interface{}) {
+	dynType := reflect.TypeOf(i)
+	if dynType == nil {
+		fmt.Println("there is no dynamic type")
+	}
+	n := dynType.NumMethod()
+	if n == 0 {
+		fmt.Printf("%s's method is empty \n", dynType)
+		return
+	}
+	fmt.Printf("%s's method set: \n", dynType)
+	for i := 0; i < n; i++ {
+		fmt.Println("-", dynType.Method(i).Name)
+	}
+}
+
 func TestPointMethod(t *testing.T) {
 
 	var a1 Tyt
@@ -26,11 +43,14 @@ func TestPointMethod(t *testing.T) {
 	a1.M2()
 	fmt.Println(a1.i)
 
+	dumpMethodSet(a1)
+
 	var a2 = &Tyt{}
 	fmt.Println(a2.i)
 	a2.M1()
 	fmt.Println(a2.i)
 	a2.M2()
 	fmt.Println(a2.i)
+	dumpMethodSet(a2)
 
 }
